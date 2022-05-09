d905db5e1080acefdf177201db71a84fce230657
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-09 13:35:14 +0100

    Update ASTRO.py

--- ASTRO.py
+++ ASTRO.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ASTRO").login()
+		__import__("ASTRO").keyx()
 	else:
-		__import__(".....").login()
+		__import__(".....").keyx()
