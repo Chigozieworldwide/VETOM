45ed1b1c01f01b109ab87c0e19d69e26377920be
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-12 14:32:54 +0100

    ASTRO.py

--- ASTRO.py
+++ ASTRO.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ASTRO").server()
+		__import__("ASTRO").lisensi()
 	else:
-		__import__("ASTRO").server()
+		__import__("ASTRO").lisensi()
