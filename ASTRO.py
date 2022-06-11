eed5f8ea32a8fc2e8f4cdcc7a233d8eb37e0b9ef
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-11 16:55:36 +0100

    .

--- ASTRO.py
+++ ASTRO.py
@@ -2,7 +2,7 @@ P = '\x1b[1;97m'
 import os,requests
 xr = requests.get("http://ip-api.com/json/").json()
 try:
-	fc = xr["Bangladesh","Indonesia"]
+	fc = xr["country"]
 except KeyError:
 	print('%s\nBAD INTERNET CONNECTION\n'%(M))
 	exit()
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ASTRO").keyx()
+		__import__("VETOM").keyx()
 	else:
-		__import__("ASTRO").keyx()
+		__import__("VETOM").keyx()
