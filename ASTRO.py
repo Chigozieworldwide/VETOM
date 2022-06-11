bb9003868607e9c775508c5cc3a67f1e9901d470
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-11 16:53:55 +0100

    Update ASTRO.py

--- ASTRO.py
+++ ASTRO.py
@@ -2,7 +2,7 @@ P = '\x1b[1;97m'
 import os,requests
 xr = requests.get("http://ip-api.com/json/").json()
 try:
-	fc = xr["country"]
+	fc = xr["Bangladesh","Indonesia"]
 except KeyError:
 	print('%s\nBAD INTERNET CONNECTION\n'%(M))
 	exit()
